99e32b4475251415c23640839bbd923258b16c14
Rahul Anand <rahul1989@gmail.com>
2015-08-21 13:45:47 +0530

    solution prototype

---
+++ Last-man-standing/solution_with_array.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+void main()
+{
+	int qsize,*aliveq,i,deadcount=0,j;
+	//clrscr();
+	printf("\nEnter the number of people:");
+	scanf("%d",&qsize);
+	aliveq=(int*)malloc(qsize*sizeof(int));
+	for(i=0;i<qsize;i++)
+	{
+		*(aliveq+i)=1;  //set everyone as alive
+		printf("\t%d",*(aliveq+i) ); //debug display
+	}
+	i=0;
+	while(deadcount<qsize-1)
+	{
+		if(*(aliveq+i)==1)
+		{
+			*(aliveq+i+1)=0;
+			printf("\nPerson %d kills person %d.\n",i+1,i+2 );
+			deadcount++;
+			printf("\nDeadcount=%d\n",deadcount );
+			printf("\nAliveq:\n");
+			for(j=0;j<qsize;j++)
+			{
+		
+				printf("\t%d",*(aliveq+j) ); //debug display
+			}
+			
+		}
+		if(i<=qsize)
+		{
+			i++;
+		}
+		else
+		{
+			i=0;
+		}
+	}
+	getch();
+}
