eb6208659fa3e093300f82803b3f6bbd6dd02031
Rahul Anand <rahul1989@gmail.com>
2015-09-03 22:38:51 +0530

    Dynamic allocation

--- Matrix-determinant/Solution.c
+++ Matrix-determinant/Solution.c
@@ -3,9 +3,15 @@
 
 void main()
 {
-	int n;
+	int n,**matptr,i;
 	printf("Enter the order of the square matrix:\n");
 	scanf("%d",&n);
+	matptr=(int**)calloc(n*sizeof(int*));
+	for(i=0;i<n;i++)
+	{
+		matptr[i]=(int*)calloc(n*sizeof(int));
+	}
+
 	printf("Enter the elements:\n");
 	
 }
