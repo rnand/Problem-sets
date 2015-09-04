2e5487cfb50aa2b9c976cd56596df6786ee63839
Rahul Anand <rahul1989@gmail.com>
2015-09-04 21:48:57 +0530

    Input

--- Matrix-determinant/Solution.c
+++ Matrix-determinant/Solution.c
@@ -3,15 +3,35 @@
 
 void main()
 {
-	int n,**matptr,i;
+	int n,**matptr,i,j;
 	printf("Enter the order of the square matrix:\n");
 	scanf("%d",&n);
-	matptr=(int**)calloc(n*sizeof(int*));
+	matptr=(int**)malloc(n*sizeof(int*));
 	for(i=0;i<n;i++)
 	{
-		matptr[i]=(int*)calloc(n*sizeof(int));
+		matptr[i]=(int*)malloc(n*sizeof(int));
 	}
-
 	printf("Enter the elements:\n");
-	
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			scanf("%d",&matptr[i][j]);
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("\n");
+		for(j=0;j<n;j++)
+		{
+			printf("%d\t",matptr[i][j]);
+		}
+	}
+	//do stuff here
+	for(i=0;i<n;i++)
+	{
+		free(matptr[i]);
+	}
+	free(matptr);
+
 }
