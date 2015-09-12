8248c9d54bc98d5b3e4260b3118e89695de5d6b1
Rahul Anand <rahul1989@gmail.com>
2015-09-12 18:54:49 +0530

    Solution with linked list framework

---
+++ Last-man-standing/Solution_with_linked_lists.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct node
+{
+ //define the node here
+}NODE;
+
+int* add_node(int* head)
+{
+	//add body
+}
+
+int* find_next(int* head)
+{
+	//add body
+}
+
+void main()
+{
+	int n;
+	printf("\nEnter the number of people:\n");
+	scanf("%d",&n);
+}
